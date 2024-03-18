032437e15f8f9bb295f35b2590406ffc3e9e98b1
deniseschirmer <deniseschirmerr@gmail.com>
2024-03-18 06:25:50 -0300

    finalizing login screen and creating signup screen

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,34 +1,40 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/home.module.scss";
+import styles from "../../styles/home.module.scss";
 
-import logoImg from "../../public/logo.svg";
+import logoImg from "../../../public/logo.svg";
 
-import { Input } from "../components/ui/Input";
-import { Button } from "../components/ui/Button";
+import { Input } from "../../components/ui/Input";
+import { Button } from "../../components/ui/Button";
+
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>SujeitoPizza - Faça seu login</title>
+        <title>Faça seu cadastro agora!</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
 
         <div className={styles.login}>
+          <h1>Criando sua conta</h1>
+
           <form>
+            <Input placeholder="Digite seu nome" type="text" />
+
             <Input placeholder="Digite seu email" type="text" />
 
             <Input placeholder="Sua senha" type="password" />
 
             <Button type="submit" Loading={false}>
-              Acessar
+              Cadastrar
             </Button>
           </form>
-          <Link href="/signup" target="_blank" passHref>
-            <p className={styles.text}> Não possui uma conta? Cadastra-se</p>
+
+          <Link href="/" target="_blank">
+            <p className={styles.text}>Já possui uma conta? Faça login!</p>
           </Link>
         </div>
       </div>
