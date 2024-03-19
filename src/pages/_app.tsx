18c6c4506f70a2d3589f57b060ac5b4d7486e2a1
deniseschirmer <deniseschirmerr@gmail.com>
2024-03-19 06:40:39 -0300

    feat: sending data to login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "../styles/globals.scss";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { AuthProvider } from "../contexts/AuthContext";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
+
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { FormEvent, useContext, useState } from "react";
 import Head from "next/head";
+
 import Image from "next/image";
 import styles from "../styles/home.module.scss";
 
@@ -6,9 +8,30 @@ import logoImg from "../../public/logo.svg";
 
 import { Input } from "../components/ui/Input";
 import { Button } from "../components/ui/Button";
+
+import { AuthContext } from "@/contexts/AuthContext";
+
 import Link from "next/link";
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email,
+      password,
+    };
+
+    await signIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -19,9 +42,19 @@ export default function Home() {
 
         <div className={styles.login}>
           <form>
-            <Input placeholder="Digite seu email" type="text" />
+            <Input
+              placeholder="Digite seu email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-            <Input placeholder="Sua senha" type="password" />
+            <Input
+              placeholder="Sua senha"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
             <Button type="submit" Loading={false}>
               Acessar
